858ecdc2127473b5f7f477ecd6bb3a84e72e1aa5
MihaiVatavu <66440300+MihaiVatavu@users.noreply.github.com>
2020-09-10 00:41:12 +0100

    Portofolio Page
    Portofolio page

---
+++ style.js
@@ -0,0 +1,10 @@
+$("#typed").typed({
+  strings: ["#front-end developer", "#responsive-design", "#javascript", "#jquery"],
+  typeSpeed: 100,
+  startDelay: 0,
+  backSpeed: 60,
+  backDelay: 2000,
+  loop: true,
+  cursorChar: "",
+  contentType: 'Html'
+});
